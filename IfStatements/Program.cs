e2ecc8d7943b69de042c592af5624d619de6aae8
Jaclyn Foster <jaclynfoster89@gmail.com>
2023-09-12 17:13:04 -0500

    added notes for if statements and tryparse

--- IfStatements/Program.cs
+++ IfStatements/Program.cs
@@ -4,7 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("What is the temp outside?");
+        string temp = Console.ReadLine();
+        int numTemp = int.Parse(temp);
+
+        if (numTemp < 70)
+        {
+            Console.WriteLine("Take a coat!");
+        }
+        else if (numTemp == 70)
+        {
+            Console.WriteLine("It is just right outside.");
+        }
+         else
+        {
+            Console.WriteLine("It is a little warmer outside!");
+        }
+
+        Console.Read();
+
+        //tryParse is used when a user enters input and needs to convert a string to integer
+        string numAsString = "128";
+        int parsedValue;
+        bool success = int.TryParse(numAsString, out parsedValue);
+        //the return boolean is true when parsing was successful
+        if(success)
+        {
+            Console.WriteLine("Parsing successful-number is " + parsedValue);
+        }
+        else
+        {
+            Console.WriteLine("Parsing Failed");
+        }
     }
 }
 
